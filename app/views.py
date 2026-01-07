6b55e8a889032d23313d29dd2f650e61b33babe2
rodrigoogalde <rodrigoogalde2001@gmail.com>
2026-01-07 16:02:49 -0300

    feat: Add pagination and styling for portfolios and stocks in home view

--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
-from .models import Portfolio, Stock, Holding, TargetAllocation
+from django.core.paginator import Paginator
+from django.db.models import Prefetch
+from .models import Portfolio, Stock, Holding, TargetAllocation, StockPrice
 
 # Create your views here.
 def home(request):
-    portfolios = Portfolio.objects.all()
-    stocks = Stock.objects.all()
+    portfolios_list = Portfolio.objects.all()
+    portfolios_paginator = Paginator(portfolios_list, 10)
+    portfolios_page = request.GET.get('portfolios_page', 1)
+    portfolios = portfolios_paginator.get_page(portfolios_page)
+    
+    stocks_list = Stock.objects.prefetch_related(
+        Prefetch('prices', queryset=StockPrice.objects.order_by('-date')[:1], to_attr='latest_price')
+    ).all()
+    stocks_paginator = Paginator(stocks_list, 20)
+    stocks_page = request.GET.get('stocks_page', 1)
+    stocks = stocks_paginator.get_page(stocks_page)
+    
     return render(request, 'home.html', {
         'portfolios': portfolios,
         'stocks': stocks
