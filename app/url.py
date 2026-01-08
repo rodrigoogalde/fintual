8728c8ab595b1c54c980781a084c5773885ee502
rodrigoogalde <rodrigoogalde2001@gmail.com>
2026-01-08 19:24:02 -0300

    feat: Enhance portfolio management with buy/sell functionality and modal dialogs

--- app/url.py
+++ app/url.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, portfolio_detail, get_portfolio_balance, buy_stock, simulate_time, stock_detail
+from .views import home, portfolio_detail, get_portfolio_balance, buy_stock, sell_stock, simulate_time, stock_detail, rebalance_portfolio
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,5 +7,7 @@ urlpatterns = [
     path('stock/<int:stock_id>/', stock_detail, name='stock_detail'),
     path('api/portfolio/<int:portfolio_id>/balance/', get_portfolio_balance, name='get_portfolio_balance'),
     path('api/buy-stock/', buy_stock, name='buy_stock'),
+    path('api/sell-stock/', sell_stock, name='sell_stock'),
+    path('api/rebalance-portfolio/', rebalance_portfolio, name='rebalance_portfolio'),
     path('api/simulate-time/', simulate_time, name='simulate_time'),
 ]
